5c95b8b2273440c156fe61024dc726e7b2917da4
Max Wittmann <mwittman@thoughtworks.com>
2014-03-02 11:16:44 +1100

    Initial commit

---
+++ src/TestClass.java
@@ -0,0 +1,14 @@
+
+public class TestClass {
+	public static void main (String [] args) { 
+		
+	}
+	
+	public int doSomething1() { 
+		return 1;
+	}
+	
+	public String doSomething2() { 
+		return "2";
+	}
+}
